2f7a554d817946b266133caef71fea0677b4b8e1
Amit Samra <asamra@zipcodes-MacBook-Pro.local>
2020-03-04 10:25:02 -0500

    Solutions in each file

---
+++ answers/exercise13.sql
@@ -0,0 +1,3 @@
+ALTER TABLE Users
+CHANGE FirstName First_Name varchar(255),
+CHANGE LastName Last_Name varchar(255);
---
+++ answers/exercise15.sql
@@ -0,0 +1,7 @@
+INSERT INTO Students(StudentName, Address, City, PostalCode, Country)
+VALUES ('Jane Doe', '57 Union St', 'Glasgow', 'G13R8', 'Scotland');
+
+# or 
+
+INSERT INTO Students
+VALUES ('Jane Doe', '57 Union St', 'Glasgow', 'G13R8', 'Scotland');
